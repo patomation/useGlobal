92554c61056811a190b4df7fc328a0624928b8a1
patomation <patomation@gmail.com>
2020-04-04 14:45:22 +0700

    first commit of useGlobal code from my other project

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react'
 import { mount } from 'enzyme'
 
-import { MyComponent } from './index'
+import { useGlobal } from './index'
 
-describe('<MyComponent />', () => {
-  it('renders', () => {
-    mount(<MyComponent />)
+const Component = (): React.ReactElement => {
+  const [state, setState] = useGlobal({ color: 'red' })
+
+  const { color } = state
+
+  return (
+    <button
+      onClick={() => {
+        setState({
+          color: 'green'
+        })
+      }}
+      style={{
+        color: color as string
+      }}>
+      {color}
+    </button>
+  )
+}
+
+describe('useGlobal', () => {
+  it('renders and clicks to change color', () => {
+    const component = mount(<Component />)
+    expect(component.find('button').props().style.color).toEqual('red')
+    component.simulate('click')
+    expect(component.find('button').props().style.color).toEqual('green')
   })
 })
